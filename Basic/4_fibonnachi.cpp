45f077666f62c0f41d65f3f0a6a0dc2a362f53b0
Hans Figo <hambasahaya0303@gmail.com>
2022-12-04 23:56:00 +0700

    modified:   Basic/4_fibonnachi.cpp

--- Basic/4_fibonnachi.cpp
+++ Basic/4_fibonnachi.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
 
+int row;
+void fibonnachi(int row);
+void Reversefibonnachi(int row);
+char loop = 'y';
+
 int main()
 {
-    int x = 5;
-    for (int i = 0; i < x; i++)
+
+    do
     {
-        for (int j = 0; j < i; j++)
+        printf("Enter Row :");
+        std::cin >> row;
+
+        fibonnachi(row);
+
+        for (int z = 0; z <= row; z++)
+        {
+            printf("*");
+        }
+        printf("\n");
+
+        Reversefibonnachi(row);
+        printf("\n");
+        printf("Ulang ?? (y/n)");
+        std::cin >> loop;
+    } while (loop == 'y');
+
+    return 0;
+}
+
+void fibonnachi(int row)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j <= i; j++)
         {
             printf("*");
         }
         printf("\n");
     }
+}
 
-    return 0;
+void Reversefibonnachi(int row)
+{
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = row; j > i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
