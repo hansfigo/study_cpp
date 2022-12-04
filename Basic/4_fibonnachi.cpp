3a2f28f79b2bfd6cd1f166f2594df3c685fe904d
Hans Figo <hambasahaya0303@gmail.com>
2022-12-04 23:32:19 +0700

    modified:   Basic/4_fibonnachi.cpp

--- Basic/4_fibonnachi.cpp
+++ Basic/4_fibonnachi.cpp
@@ -3,8 +3,14 @@
 int main()
 {
     int x = 5;
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
 
-   
-    
     return 0;
 }
