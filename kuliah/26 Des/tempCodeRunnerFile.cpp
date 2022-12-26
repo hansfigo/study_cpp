64314dbafdff41270728acb67809a164fbc5b62a
Hans Figo <hambasahaya0303@gmail.com>
2022-12-26 23:01:16 +0700

    organizing

--- kuliah/26 Des/tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-    // printf("\n%d ", y1);
-    // printf("\n%d", y2);
-    // printf("\n%d", y3);
--- kuliah/Mining Game/tempCodeRunnerFile.cpp
+++
@@ -1,4 +0,0 @@
-    cout << "Hello!!\nWelcome to our minigames\n";
-    Sleep(600);
-    cout << "Please input your name : ";
-    getline(cin, userName);
